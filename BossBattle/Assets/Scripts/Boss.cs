bc87cc550fc9b3cf2ac0b9a18d9bae9c0148ac06
Heidi Erwin <heidianneerwin@gmail.com>
2019-02-16 15:43:57 -0500

    confidence now associated w/ boss, started boss script

---
+++ BossBattle/Assets/Scripts/Boss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] private float confidence = 1.0f; // confidence ranges from 0.0.to 1.0
+    [SerializeField] private int tasksHandling; // tracks number of things the boss is currently working on
+    [SerializeField] private int numTasksCapableOfHandling; // max number of tasks boss can handle before being overwhelmed
+    [SerializeField] private ConfidenceBar confidenceBar;
+    [SerializeField] private GameController gameController;
+
+
+    void Update()
+    {
+        if (tasksHandling > numTasksCapableOfHandling) {
+            SetConfidence(confidence - .001f);
+            confidenceBar.SetConfidence(confidence);
+        }
+    }
+
+    public void SetConfidence(float conf) {
+        if (confidence >= 0) {
+            confidence = conf;
+            confidenceBar.SetConfidence(conf);
+            if (confidence <= 0) {
+                gameController.LoseState(0);
+            }
+        }
+    }
+
+    public float GetConfidence() {
+        return confidence;
+    }
+}
--- BossBattle/Assets/Scripts/GameController.cs
+++ BossBattle/Assets/Scripts/GameController.cs
@@ -1,24 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private ConfidenceBar confidenceBar;
+    private Boss boss;
+    [SerializeField] GameObject confidenceTooLow;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        boss = GameObject.Find("Boss").GetComponent<Boss>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // placeholder to test confidence bar
-
         if (Input.GetKey(KeyCode.Space)) {
-            confidenceBar.SetConfidence(confidenceBar.GetConfidence() - .01f);
+            boss.SetConfidence(boss.GetConfidence() - .01f);
+            
         }
+    }
+
+    /**
+     * A method called when the game ends because the player has failed
+     * If reason is 0, the boss's confidence dropped to zero.
+     * If reasons is 1, you failed to meet your quota. 
+     */
+    public void LoseState(int reason) {
+        if (reason == 0) {
+            DisplayConfidenceTooLow();
+        }
+    }
+
+    private void DisplayConfidenceTooLow() {
+        confidenceTooLow.SetActive(true);
     }
 }
