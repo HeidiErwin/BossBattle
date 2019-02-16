169c7c7442efb301e0075c2631e7dfe9d26aa7ee
Brendan Le <le.brendan@gmail.com>
2019-02-16 15:00:41 -0500

    Add gitignore

---
+++ BossBattle/Assets/Scripts/ConfidenceBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfidenceBar : MonoBehaviour
+{
+    Transform bar;
+    private float confidence = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetConfidence(float conf) {
+        confidence = conf;
+        bar.localScale = new Vector3(conf, 1f);
+    }
+
+    public float GetConfidence() {
+        return confidence;
+    }
+}
