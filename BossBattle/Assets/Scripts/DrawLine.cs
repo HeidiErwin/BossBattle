ef07ecf825f43c8bbda1cc91a8dc0ed03b036bd5
Heidi Erwin <heidianneerwin@gmail.com>
2019-03-09 14:46:05 -0500

    added graph

---
+++ BossBattle/Assets/Scripts/DrawLine.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private float counter;
+    private float dist;
+
+    private float lineDrawSpeed = 10f;
+    private Transform start;
+    private Transform destination;
+
+    private GameObject graphBottomLeft;
+
+    private bool isDrawing = false;
+
+    void Start()
+    {
+        graphBottomLeft = GameObject.Find("GraphBottomLeft");
+        lineRenderer = GetComponent<LineRenderer>();
+        if (!start) {
+            start = graphBottomLeft.transform;
+        } 
+        lineRenderer.SetPosition(0, start.position);
+        lineRenderer.startWidth = .05f;
+        lineRenderer.endWidth = .05f;
+    }
+
+    void FixedUpdate()
+    {
+        if (start && destination) {
+            dist = Vector3.Distance(start.position, destination.position);
+        }
+
+        if ((isDrawing) && (counter < dist)) {
+            counter += .1f / lineDrawSpeed;
+
+            // lerp = linear interpolation, will give us value of counter but max bound dist and min bound 0
+            float len = Mathf.Lerp(0, dist, counter);
+
+            // animate the line being drawn
+            Vector3 pointA = start.position;
+            Vector3 pointB = destination.position;
+            Debug.Log("here");
+
+            // get unit vector in desired direction, multiple by desired length, add start point
+            Vector3 pointAlongLine = len * Vector3.Normalize(pointB - pointA) + pointA;
+
+            lineRenderer.SetPosition(1, pointAlongLine); 
+        } else if (isDrawing) {
+            isDrawing = false;
+            start = destination;
+        }
+    }
+
+    public void SetDest(Transform dest) {
+        destination = dest;
+    }
+
+    public void SetStart(Transform s) {
+        start = s;
+    }
+
+    public void SetDrawing(bool b) {
+        isDrawing = b;
+    }
+
+    public Transform GetEnd() {
+        return destination;
+    }
+}
---
+++ BossBattle/Assets/Scripts/Graph.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    private GameObject timeMarker;
+    [SerializeField] private DrawLine line; // the dynamic line that gets drawn at certain intervals
+    private int numLineSegments = 5;
+    private float distToMoveTimeMarkerPerSec;
+    private float timer = 0.0f;
+    private float graphWidth;
+    private float graphHeight;
+    private GameObject mostRecentPoint;
+    private float markerMovementThroughCurrentSection = 0;
+    
+    // these will go in controller but someone else editing that script rn
+    private int tasksFinished = 0; 
+    private int totalPossibleTasks = 10;
+    private float secondsPerLevel = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeMarker = GameObject.Find("TimeMarker");
+
+        graphWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        graphHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+        Debug.Log("height = " + graphHeight);
+        distToMoveTimeMarkerPerSec = graphWidth/secondsPerLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeMarker.transform.position = 
+            new Vector3(timeMarker.transform.position.x + distToMoveTimeMarkerPerSec * Time.deltaTime,
+            timeMarker.transform.position.y, timeMarker.transform.position.z);
+
+        markerMovementThroughCurrentSection += distToMoveTimeMarkerPerSec * Time.deltaTime;
+
+        if (markerMovementThroughCurrentSection >= graphWidth/numLineSegments) {
+            PlaceDotOnGraph(timeMarker.transform.position.x);
+
+            Transform prevLineEnd = line.GetEnd();
+            GameObject segment = new GameObject();
+            segment.SetActive(false);
+            segment = Instantiate(Resources.Load("LineRenderer")) as GameObject;
+            line = segment.GetComponent<DrawLine>();
+            line.SetStart(prevLineEnd);
+            line.SetDest(mostRecentPoint.transform);
+            segment.SetActive(true);
+            line.SetDrawing(true);
+            markerMovementThroughCurrentSection = 0;
+        }
+
+        // change later, this is for testing that graph responds to finished tasks
+        if (Input.GetKeyDown(KeyCode.T)) {
+            tasksFinished++;
+            PlaceDotOnGraph(timeMarker.transform.position.x);
+        }
+    }
+
+    private void PlaceDotOnGraph(float timeMarkerX) {
+        GameObject point = Instantiate(Resources.Load("Point")) as GameObject;
+        float pointX = timeMarkerX;
+        float graphBottomLeft = this.transform.position.y - graphHeight / 2;
+        float pointY = graphBottomLeft + (graphHeight / totalPossibleTasks * tasksFinished);
+        point.transform.position = new Vector3(pointX, pointY, 0);
+        mostRecentPoint = point;
+    }
+
+    public float GetWidth() {
+        return GetComponent<Sprite>().bounds.size.x;
+    }
+}
