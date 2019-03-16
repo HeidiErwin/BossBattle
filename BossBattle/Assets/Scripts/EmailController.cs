367f340c1f3516983b1392a8d43d478ea99c7682
Heidi Erwin <heidianneerwin@gmail.com>
2019-03-16 15:38:02 -0400

    email functionality started

---
+++ BossBattle/Assets/Scripts/EmailController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmailController: MonoBehaviour
+{
+    public GameObject currentEmail;
+    private bool displayingEmail = false;
+    public List<GameObject> inbox;
+
+    public Sprite closed;
+    public Sprite newMessage;
+    public Sprite open;
+    public Animation flashing;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (!displayingEmail && inbox.Count > 0) {
+                currentEmail = inbox[0];
+                currentEmail.SetActive(true);
+                displayingEmail = true;
+                //GetComponent<Image>().sprite = open;
+                inbox.RemoveAt(0);
+            } else if (!displayingEmail && inbox.Count <= 0) {
+                // todo: play "inbox empty!" error sound
+            }
+            else {
+                currentEmail.SetActive(false);
+                displayingEmail = false;
+                GetComponent<Image>().sprite = closed;
+            }
+        }
+    }
+
+    void AddEmailToInbox (GameObject email) {
+        inbox.Add(email);
+    }
+}
